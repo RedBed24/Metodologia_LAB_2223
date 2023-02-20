32d2ab6d25dd5396193779b7693c67d118903434
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-02-20 14:11:21 +0100

    Formateado del código y prints para controlar la ejecución

--- EnClase/src/Sesion1.java
+++ EnClase/src/Sesion1.java
@@ -4,95 +4,69 @@ import profes.leer;
 public class Sesion1 {
 
 	public static void main(String[] args) {
-		String[] fotoEntrada = { "1024x1024.png", "1536x1536.png", "320x214.png", "640x360.png", "640x427.png" };
-		long[][] tiempoPasado = new long[fotoEntrada.length][5];
+		String[] fotoEntrada = { "320x214.png", "640x360.png", "640x427.png", "1024x1024.png", "1536x1536.png",  };
+		long[][] tiempoPasado = new long[5][fotoEntrada.length];
+
 		String fotoSalida;
 		long tiempoActual;
+
 		int[] histograma = null;
-		boolean calcularTiempoNano = 's' == Character.toLowerCase(leer.caracter(
-				"¿Quieres calcular el tiempo en Nanosegundos? ['S', cualier otra resppuesta se medirá en milisegundos]:"));
-		boolean salir = false;
 
-		do {
-			try {
-				/*
-				 * switch (Character.toLowerCase(leer.caracter(
-				 * "Elige un apartado: ['A', 'B', 'C', 'D', 'S'] (Selecionar 'S' sale del programa):"
-				 * ))) { case 'a':
-				 */
-				for (int i = 0; i < fotoEntrada.length; i++) {
-					/* = leer.cadena("Introduce el nombre de la foto del apartado A:"); */
+		boolean calcularTiempoNano = 's' == Character.toLowerCase(leer.caracter("¿Quieres calcular el tiempo en Nanosegundos? ['S', cualier otra resppuesta se medirá en milisegundos]:"));
 
-					if (!fotoEntrada[i].endsWith(".png")) {
-						System.err.println("El nombre de la imagen no tiene el formato esperado");
-						throw new Exception();
-					}
+		try {
+			for (int i = 0; i < fotoEntrada.length; i++) {
+				
+				System.out.println("Processing " + fotoEntrada[i] + "...");
 
-					fotoSalida = fotoEntrada[i].substring(0, fotoEntrada[i].length() - 4) + "_g.png";
+				System.out.println("Empezando apartado a");
 
-					tiempoActual = calcularTiempoNano ? System.nanoTime() : System.currentTimeMillis();
-					Auxiliar.GenerarImagenGrises(fotoEntrada[i], fotoSalida);
-					tiempoPasado[i][0] = calcularTiempoNano ? System.nanoTime()
-							: System.currentTimeMillis() - tiempoActual;
-				/*
-				 * break; case 'b':
-				 */
-					/* String ruta = leer.cadena("Escribe la ruta de la foto del apartado B: "); */
-					tiempoActual = calcularTiempoNano ? System.nanoTime() : System.currentTimeMillis();
-					histograma = Auxiliar.HistogramaImagen(fotoEntrada[i]);
-					tiempoPasado[i][1] = calcularTiempoNano ? System.nanoTime()
-							: System.currentTimeMillis() - tiempoActual;
-
-				/*
-				 * break; case 'c':
-				 */
-					if (histograma == null) {
-						System.err.println(
-								"El histograma todavía no está inicializado, por favor, usa el apartado 'B' antes.");
-						throw new Exception();
-					}
-
-					System.out.println("El histograma de la imagen anterior es:");
-					tiempoActual = calcularTiempoNano ? System.nanoTime() : System.currentTimeMillis();
-					Auxiliar.ImprimeHistograma(histograma);
-					tiempoPasado[i][2] = calcularTiempoNano ? System.nanoTime()
-							: System.currentTimeMillis() - tiempoActual;
-				/*
-				 * break; case 'd':
-				 
-				String fotoEntradaD = leer.cadena("Introduce el nombre de la foto del apartado D:");*/
-
-				if (!fotoEntrada[i].endsWith(".png")) {
-					System.err.println("El nombre de la imagen no tiene el formato esperado");
-					throw new Exception();
-				}
-
-				for (int método = 0; método <= 1; método++) {/*leer.entero("Introduce el método deseado:\n0: BubbleSort.\n1: QuickSort.\n", 0, 1);*/
-
-				fotoSalida = fotoEntrada[i].substring(0, fotoEntrada[i].length() - 4);
-				switch (método) {
-				case 0:
-					fotoSalida += "_b.png";
-					break;
-				case 1:
-					fotoSalida += "_q.png";
-					break;
-				}
+				fotoSalida = fotoEntrada[i].substring(0, fotoEntrada[i].length() - 4) + "_g.png";
 
 				tiempoActual = calcularTiempoNano ? System.nanoTime() : System.currentTimeMillis();
-				Auxiliar.GenerarImagenOrdenandoColumnas(fotoEntrada[i], fotoSalida, método);
-				tiempoPasado[i][3+método] = calcularTiempoNano ? System.nanoTime() : System.currentTimeMillis() - tiempoActual;
-				}
-				/*
-				 * break; case 's': salir = true; break; default:
-				 * System.err.println("El apartado no existe."); throw new Exception(); }
-				 */
-				}
-			} catch (Exception e) {
-				System.err.print("Error");
-			}
+				Auxiliar.GenerarImagenGrises(fotoEntrada[i], fotoSalida);
+				tiempoPasado[0][i] = calcularTiempoNano ? System.nanoTime() : System.currentTimeMillis() - tiempoActual;
 
-	}while(!salir);System.out.println("Ending program.");
+				System.out.println("Empezando apartado b");
+
+				tiempoActual = calcularTiempoNano ? System.nanoTime() : System.currentTimeMillis();
+				histograma = Auxiliar.HistogramaImagen(fotoEntrada[i]);
+				tiempoPasado[1][i] = calcularTiempoNano ? System.nanoTime() : System.currentTimeMillis() - tiempoActual;
+
+				System.out.println("Empezando apartado c");
+
+				tiempoActual = calcularTiempoNano ? System.nanoTime() : System.currentTimeMillis();
+				Auxiliar.ImprimeHistograma(histograma);
+				tiempoPasado[2][i] = calcularTiempoNano ? System.nanoTime() : System.currentTimeMillis() - tiempoActual;
+
+				System.out.println("Empezando apartado d");
+
+				for (int método = 0; método <= 1; método++) {
+					System.out.println("Usando método " + método + "en apartado d");
+
+					fotoSalida = fotoEntrada[i].substring(0, fotoEntrada[i].length() - 4);
+					switch (método) {
+					case 0: fotoSalida += "_b.png"; break;
+					case 1: fotoSalida += "_q.png"; break;
+					}
+
+					tiempoActual = calcularTiempoNano ? System.nanoTime() : System.currentTimeMillis();
+					Auxiliar.GenerarImagenOrdenandoColumnas(fotoEntrada[i], fotoSalida, método);
+					tiempoPasado[3+método][i] = calcularTiempoNano ? System.nanoTime() : System.currentTimeMillis() - tiempoActual;
+				}
+				
+				System.out.println("Se ha terminado de procesar la imagen " + fotoEntrada[i]);
+			}
+			
+			System.out.println("Generando fichero datos.csv");
+			IO.generarCSV("datos.csv", tiempoPasado);
+
+			System.out.println("Finalización correcta");
+		} catch (Exception e) {
+			System.err.print("Error");
+		}
+
+		System.out.println("Ending program.");
 
   }
 }
