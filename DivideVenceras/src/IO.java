1bc7389e57661f49b08c3ed530917da0adc1bec6
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-02-27 18:02:50 +0100

    Añadida documentación a la clase IO

--- DivideVenceras/src/IO.java
+++ DivideVenceras/src/IO.java
@@ -5,21 +5,47 @@ import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
+import excepciones.CarCreationException;
+
+/**
+ * <p>
+ * Clase que se encarga de la salida y entrada en ficheros.
+ * </p>
+ * @author Noelia Díaz-Alejo Alejo, Samuel Espejo Gil 
+ * @version 0.01
+ * @since 2023-02-27
+ */
 public class IO {
 	
+	/**
+	 * <p>
+	 * Método encargado de leer el fichero que contiene los datos.
+	 * </p>
+	 * @param pahtname El camino y nombre al fichero a leer
+	 * @param coches Lista <b>ya creada</b> que contendrá los coches que se leerán del fichero
+	 * @throws FileNotFoundException En caso de no encontrar el fichero con el pathname dado
+	 */
 	public static void leerCoches(final String pahtname, final List <Coche> coches) throws FileNotFoundException {
+		/* Declaración del scanner a usar para leer las líneas */
 		final Scanner input = new Scanner(new File(pahtname));
+
 		StringTokenizer st;
+
+		/* Variable que contendrá el token cuando sea necesario */
 		String token;
 		
 		while (input.hasNextLine()) {
 			try {
+				/* Vamos obteniendo los tokens los cuales están separados por "," */
 				st = new StringTokenizer(input.nextLine(), ",");;
 				
+				/* Token modelo */
 				String modelo = st.nextToken();
 				
+				/* Guardamos el token temporalmente */
 				token = st.nextToken();
 				
+				/* Intentamos obtener el tipo de combustible a partir del token */
 				Coche.TipoCombustible comb;
 				switch (token.toLowerCase()) {
 					case "petrol": comb = Coche.TipoCombustible.PETROL; break;
@@ -27,14 +53,15 @@ public class IO {
 					default: throw new IllegalArgumentException("Unexpected value: " + token);
 				}
 				
+				/* Pasamos a entero porque en el fichero se encuentra como doble, pero no tiene sentido que este no sea entero */
 				int asientos = (int) Double.parseDouble(st.nextToken());
 				
 				token = st.nextToken();
 
 				Coche.TipoTansmisión trans;
 				switch (token.toLowerCase()) {
-					case "autoken": trans = Coche.TipoTansmisión.AUTOMÁTICO; break;
-					case "manual":  trans = Coche.TipoTansmisión.MANUAL;     break;
+					case "automatic": trans = Coche.TipoTansmisión.AUTOMÁTICO; break;
+					case "manual":    trans = Coche.TipoTansmisión.MANUAL;     break;
 					default: throw new IllegalArgumentException("Unexpected value: " + token);
 				}
 				
@@ -42,6 +69,7 @@ public class IO {
 
 				double consumoMedio = Double.parseDouble(st.nextToken());
 				
+				/* Creamos y añadimos el coche a la lista */
 				coches.add(new Coche(modelo, comb, asientos, trans, capacidad, consumoMedio));
 
 			} catch (NumberFormatException e) {
@@ -50,6 +78,8 @@ public class IO {
 				System.err.println("Error trying to read enum: " + e.getMessage());
 			} catch (NoSuchElementException e) {
 				System.err.println("Error trying to read next token.");
+			} catch (CarCreationException e) {
+				System.err.println(e.getMessage());
 			}
 		}
 		
