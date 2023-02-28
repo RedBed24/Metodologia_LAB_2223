dd00856e506439cd341d3573816297e598781ef6
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-02-28 08:32:14 +0100

    Añadida documentación Auxiliar.mínimo() y primera llamada a este

--- DivideVenceras/src/Auxiliar.java
+++ DivideVenceras/src/Auxiliar.java
@@ -1,20 +1,49 @@
 
 public class Auxiliar {
 	
-	public static int mínimo(final int[] vector, int límiteInferior, int límiteSuperior) {
-		int mid, min = 0, minIzquierda, minDerecha;
+	/**
+	 * <p>
+	 * Mínimo obtenido mediante divide y vencerás.
+	 * </p>
+	 * @param vector Vector del cuál obtendremos el mínimo
+	 * @param límiteInferior Posición inferior del trozo que nos toca mirar
+	 * @param límiteSuperior Posición superior del trozo que nos toca mirar
+	 * @return Mínimo local del trozo que nos toca mirar
+	 */
+	private static int mínimoRec(final int[] vector, int límiteInferior, int límiteSuperior) {
+		int medio, mínimo, mínimoDerecha;
 
+		/* si sólo hay 1 elemento entre el límite inferior y el superior */
 		if (límiteInferior == límiteSuperior) {
-			min = vector[límiteInferior];
-		} else if (límiteInferior < límiteSuperior) {
-			mid = (límiteInferior + límiteSuperior)/2;
-			min = mínimo(vector, límiteInferior, mid);
-			minDerecha = mínimo(vector, mid + 1, límiteSuperior);
+			mínimo = vector[límiteInferior];
+		} else  {
+			/* debemos partir por la mitad el trozo que estábamos buscando */
+			medio = (límiteInferior + límiteSuperior) / 2;
 			
-			if (minDerecha < min) min = minDerecha;
+			/* obtenemos el mínimo de cada lado */
+			mínimo = mínimoRec(vector, límiteInferior, medio);
+			mínimoDerecha = mínimoRec(vector, medio + 1, límiteSuperior);
+			
+			/* comprobamos cuál es el mínimo de esos dos */
+			if (mínimoDerecha < mínimo) mínimo = mínimoDerecha;
 		}
 
-		return min;
+		/* este será el mínimo del trozo que estábamos viendo */
+		return mínimo;
+	}
+	
+	
+	/**
+	 * <p>
+	 * Mínimo obtenido mediante divide y vencerás.
+	 * Esta se encarga de abstraer de hacer la primera llamada al algoritmo.
+	 * </p>
+	 * @param vector Vector del cuál obtendremos el mínimo
+	 * @return Mínimo del Vector
+	 */
+	public static int mínimo(final int[] vector) {
+		/* El trozo por el que debemos a empezar a mirar es [0, longitud) */
+		return mínimoRec(vector, 0, vector.length - 1);
 	}
 
 }
