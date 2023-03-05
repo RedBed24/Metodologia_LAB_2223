e5a66b3e784edcc85d77cbaebb2454a5df862cf5
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-03-05 11:48:52 +0100

    Cambiado uso de Listas por Vectores en Main. Llamada ordenación Coche

--- DivideVenceras/src/Main.java
+++ DivideVenceras/src/Main.java
@@ -1,6 +1,5 @@
 import java.io.FileNotFoundException;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Vector;
 
 import campusvirtual.leer;
 
@@ -11,11 +10,11 @@ public class Main {
 	 * Método encargado de obtener los datos para la ejecución del programa.
 	 * </p>
 	 * @param pathname Ruta y nombre al archivo de datos a leer.
-	 * @param coches Lista <i>ya creada</i> donde se añadirán los coches leídos.
+	 * @param coches Vector <i>ya creada</i> donde se añadirán los coches leídos.
 	 * @return Vector con las distancias entre los POIs.
 	 * @throws FileNotFoundException Cuando el fichero de datos a leer no se encuentra.
 	 */
-	public static double[] obtenerDatos(final String pathname, final List<Coche> coches) throws FileNotFoundException {
+	public static double[] obtenerDatos(final String pathname, final Vector<Coche> coches) throws FileNotFoundException {
 		/* leemos un entero el cual será el número de puntos de interés (numPOIs) */
 		int leído;
 		do {
@@ -43,31 +42,39 @@ public class Main {
 	 * <p>
 	 * Simula la ejecucuón del programa
 	 * </p>
-	 * @param coches Lista con los coches sobre los que se realizará la ejecución.
+	 * @param coches Vector con los coches sobre los que se realizará la ejecución.
 	 * @param distanciasPOIs Vector con las distancias en kilómetros entre los POIs
 	 */
-	public static void run(final List<Coche> coches, final double[] distanciasPOIs) {
+	public static void run(final Vector<Coche> coches, final double[] distanciasPOIs) {
 		/* TODO:
 		 * [ ] Hacer esto mediante divide y vencerás
-		 * [ ] Ordenar coches respecto a consumido (capacidad libre)
 		 */
 
 		/* para cada coche */
-		for (Coche coche : coches) {
+		for (final Coche coche : coches) {
 			/* para cada distancia */
-			for (double distancia : distanciasPOIs)
+			for (final double distancia : distanciasPOIs)
 				/* Hacemos que el coche recorra la distancia */
 				/* si el coche ya no puede recorrer más, pasamos al siguiente */
-				if (distancia != coche.recorrer(distancia)) break;
-			
-			/* Mostramos cuánto ha consumido el coche */
-			System.out.printf("El coche %s ha consumido %.3fL\n", coche, coche.getCapacidadLibre());
+				if (distancia != coche.recorrer(distancia)) {
+					/* marcamos que no ha sido capaz de recorrer todo */
+					coche.setHaRecorridoTodo(false);
+					break;
+				}
+		}
+		
+		final Vector<Coche> cochesOrdenados = Ordenar.ordenarPorConsumo(coches);
+		
+		for (final Coche coche : cochesOrdenados) {
+			System.out.printf("\nEl coche %s ha consumido %.3fL.", coche, coche.getCapacidadLibre());
+			if (!coche.isHaRecorridoTodo())
+				System.out.print(" No ha sido capaz de llegar al final.");
 		}
 	}
 
 	public static void main(String[] args) {
 		try {
-			final List<Coche> coches = new LinkedList<>();
+			final Vector<Coche> coches = new Vector<>();
 			
 			final double[] distanciasPOIs = obtenerDatos("cars_dataset.csv", coches);
 			
@@ -76,7 +83,7 @@ public class Main {
 		} catch (FileNotFoundException e) {
 			System.err.println("No se ha encontrado el fichero \"cars_dataset.csv\".");
 		} catch (Exception e) {
-			System.err.println("Error inesperado.");
+			System.err.println("Error inesperado: " + e);
 		}
 	}
 
