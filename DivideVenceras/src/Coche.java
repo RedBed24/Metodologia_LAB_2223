f8bc71fd1553155478c6349dd795e86ac1e1094d
Noelia Díaz Alejo Alejo <USER@172.19.169.9>
2023-03-13 09:49:55 +0100

    Creación de la clase enum Tansmisión

--- DivideVenceras/src/Coche.java
+++ DivideVenceras/src/Coche.java
@@ -54,34 +54,6 @@ public class Coche {
 
 	/**
 	 * <p>
-	 * Dominio:
-	 * <ul>
-	 * <li>AUTOMATIC</li>
-	 * <li>MANUAL</li>
-	 * </ul>
-	 * </p>
-	 */
-	public enum TipoTansmisión { AUTOMATIC, MANUAL; };
-	
-	/**
-	 * <p>
-	 * Método encargado de parsear un String con uno de los posibles tipos de Transmisión y devolverlo como enum.
-	 * </p>
-	 * @param token String con el tipo de transmisión que se quiere conseguir.
-	 * @return instancia del Enum con el transmisión obtenido.
-	 */
-	public static TipoTansmisión parseTipoTransmisión(final String token) {
-		TipoTansmisión trans;
-		switch (token.toLowerCase()) {
-			case "automatic": trans = TipoTansmisión.AUTOMATIC; break;
-			case "manual":    trans = TipoTansmisión.MANUAL;     break;
-			default: throw new IllegalArgumentException("Unexpected TipoTransmisión value: " + token);
-		}
-		return trans;
-	}
-	
-	/**
-	 * <p>
 	 * Crea un nuevo coche con los parametros especificados.
 	 * Rellena el tanque.
 	 * </p>
