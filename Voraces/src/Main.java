0706340bc9251224fde50f77fdda8eca739f0355
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-03-20 19:48:01 +0100

    Esbozo lectura de los datos

--- Voraces/src/Main.java
+++ Voraces/src/Main.java
@@ -1,14 +1,23 @@
+import java.io.FileNotFoundException;
 import java.util.Vector;
 
 import Vaca.Vaca;
 
 public class Main {
+	
+	public static int obtenerDatos(final String pathname, final Vector<Vaca> vacas) throws FileNotFoundException {
+		IO.leerVacas(pathname, vacas);
+
+		//aux.leer(0, vacas.size());
+		return -1;
+	}
 
 	public static void main(String[] args) {
 		final Vector<Vaca> vacas = new Vector<>(30);
 
 		try {
-			IO.leerVacas(Constantes.PATHNAME_VACAS, vacas);
+			final int vacasASeleccionar = obtenerDatos(Constantes.PATHNAME_VACAS, vacas);;
+		
 			for (Vaca vaca : vacas) 
 				System.out.println(vaca);
 		} catch (Exception e) {
