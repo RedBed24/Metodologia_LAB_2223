24e40fde2c992853ce22e370cb47ae614e27414c
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-03-21 15:53:28 +0100

    Añadida ejecución programa

--- Voraces/src/Main.java
+++ Voraces/src/Main.java
@@ -1,25 +1,82 @@
 import java.io.FileNotFoundException;
 import java.util.Vector;
 
-import Vaca.Vaca;
+import campusvirtual.leer;
+import vaca.Vaca;
 
 public class Main {
 	
 	public static int obtenerDatos(final String pathname, final Vector<Vaca> vacas) throws FileNotFoundException {
 		IO.leerVacas(pathname, vacas);
 
-		//aux.leer(0, vacas.size());
-		return -1;
+		return leer.entero("¿Cuántas vacas serán seleccionadas?", 0, vacas.size());
+	}
+	
+	public static int run(final Vector<Vaca> vacas, final Vector<Vaca> vacasSeleccionadas, final int comidaDisponible, final int cantidadVacasASeleccionar) {
+		int índiceVacaActual = 0;
+		int sumaConsumoComida = 0;
+
+		final Vector<Vaca> vacasOrdenadasPorBeneficio = Ordenar.ordenarPorRatio(vacas);
+		
+		/*
+		 * Salidas:
+		 * ya hemos gastado la comida
+		 * no quedan vacas, este será el peor caso
+		 * ya hemos seleccionado el máximo
+		 */
+		while (
+				! (sumaConsumoComida == comidaDisponible)                /* ! sea solución */
+				&& índiceVacaActual < vacasOrdenadasPorBeneficio.size()  /* nos queden vacas por seleccionar */
+				&& vacasSeleccionadas.size() < cantidadVacasASeleccionar /* podemos seleccionar más vacas */
+		) {
+			Vaca vacaActual = vacasOrdenadasPorBeneficio.get(índiceVacaActual);
+			
+			/* vemos si la vaca es viable */
+			if (sumaConsumoComida + vacaActual.getConsumoComida() <= comidaDisponible) {
+				/* en ese caso la seleccionados */
+				vacasSeleccionadas.add(vacaActual);
+				sumaConsumoComida += vacaActual.getConsumoComida();
+			} else {
+				/* TODO: Debug prints */
+				System.out.println("Vaca no añadida: " + vacaActual);
+				System.out.printf("suma %d, resto %d\n", sumaConsumoComida, comidaDisponible - sumaConsumoComida);
+			}
+			
+			++índiceVacaActual;
+		}
+		
+		/* TODO: Debug prints */
+		System.out.println("\nIteraciones: " + índiceVacaActual);
+		return comidaDisponible - sumaConsumoComida;
+	}
+
+	public static void mostrarResultados(final Vector<Vaca> vacasSeleccionadas, final int cantidadVacasASeleccionar, final int comidaNoUsada) {
+
+		System.out.printf("\nLa comida que queda sin usarse es %dkg\n", comidaNoUsada);
+		
+		
+		System.out.printf("\nLas %d vacas seleccionadas de %d que había que seleccionar son:\n", vacasSeleccionadas.size(), cantidadVacasASeleccionar);
+		System.out.println("Código Consumo Producción Ratio");
+		System.out.println("            kg          L  L/kg");
+		for (Vaca vaca : vacasSeleccionadas) {
+			System.out.println(vaca);
+		}
 	}
 
 	public static void main(String[] args) {
-		final Vector<Vaca> vacas = new Vector<>(30);
-
 		try {
-			final int vacasASeleccionar = obtenerDatos(Constantes.PATHNAME_VACAS, vacas);;
-		
-			for (Vaca vaca : vacas) 
-				System.out.println(vaca);
+			final Vector<Vaca> vacas = new Vector<>(50);
+
+			final int cantidadVacasASeleccionar = obtenerDatos(Constantes.PATHNAME_VACAS, vacas);
+
+			final Vector<Vaca> vacasSeleccionadas = new Vector<>(cantidadVacasASeleccionar);
+			
+			final int comidaNoUsada = run(vacas, vacasSeleccionadas, Constantes.COMIDA_DIARIA_DISPONIBLE, cantidadVacasASeleccionar);
+			
+			mostrarResultados(vacasSeleccionadas, cantidadVacasASeleccionar, comidaNoUsada);
+
+		} catch (FileNotFoundException e) {
+			System.err.println("No se ha encontrado el fichero \"" + Constantes.PATHNAME_VACAS + "\".");
 		} catch (Exception e) {
 			System.err.println("Ha ocurrido un error inesperado. " + e);
 		}
