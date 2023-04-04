59e62f023585a3344b7f1d2baf4c9d839f9baead
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-04-04 18:47:43 +0200

    Voraces Estudio del trabajo, complejidad optimalid
    + Estudio.md: Estudio optimalidad y complejidad del trabajo.
    
    * Valor RELACIÓN_COSTE_COMIDA_ESPACIO acorde a datos vacas.

--- Voraces/src/vaca/ordenacion/CosteEspacioYComida.java
+++ Voraces/src/vaca/ordenacion/CosteEspacioYComida.java
@@ -5,12 +5,12 @@ import java.util.Comparator;
 import vaca.Vaca;
 
 public class CosteEspacioYComida implements Comparator<Vaca> {
-	/* la comida cuesta 10 veces más que el espacio, esto hace que estén al mismo nivel
+	/* la comida cuesta 8.7 veces más que el espacio, esto hace que estén al mismo nivel, que el precio tenga en cuenta por igual uno y otro
 	 * Aumentando este hacemos que la comida se valore más que el espacio, es decir, se tenga más en cuenta
 	 * rango = [0, infinito)
 	 * 0 hace que no se tenga en cuenta la comida
 	 * un valor muy alto hace (prácticamente) que no se tenga en cuenta el espacio */
-	final static double RELACIÓN_COSTE_COMIDA_ESPACIO = 10;
+	final static double RELACIÓN_COSTE_COMIDA_ESPACIO = 8.7;
 
 	@Override
 	public int compare(final Vaca o1, final Vaca o2) {
@@ -20,3 +20,4 @@ public class CosteEspacioYComida implements Comparator<Vaca> {
 	}
 
 }
+
