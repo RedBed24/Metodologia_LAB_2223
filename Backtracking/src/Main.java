62bb423bbad1989f3baecc846d07fc4a50349065
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-04-18 19:59:44 +0200

    Mejora Main Backtracking, uso Registro, ordenación Vaca y única llamada

--- Backtracking/src/Main.java
+++ Backtracking/src/Main.java
@@ -5,95 +5,34 @@ import vaca.Vaca;
 
 public class Main {
 
-	public static boolean runA (
-			final Vector<Vaca> vacasDisponibles,
-			final double lecheDeseada,
-			final int límiteEspacio,
-			final int etapa,
-			final Solucion solucion
-	) {
-		boolean soluciónEncontrada = lecheDeseada <= 0;
-		
-		// si no hemos llegado a una solución
-		if (!soluciónEncontrada) {
-			// si no quedan más vacas disponibles
-			if (etapa == vacasDisponibles.size()) {
-				// no se ha llegado a una solución por aquí
-
-			// si quedan vacas disponibles
-			} else {
-				// obtenemos la vaca correspondiente
-				Vaca vacaEtapa = vacasDisponibles.elementAt(etapa);
-
-				// TODO: Comprobar si es mejor ir añadiendo las vacas "a la que volvemos", primero por la rama del null
-
-				// vemos si es viable
-				if (vacaEtapa.getOcupaEspacio() <= límiteEspacio) {
-					solucion.añadirVaca(vacaEtapa, etapa); 
-
-					soluciónEncontrada = runA(
-							vacasDisponibles,
-							// actualizamos la cantidad de leche deseada
-							lecheDeseada - vacaEtapa.getProducciónLeche(),
-							// actualizamos el espacio que gastamos
-							límiteEspacio - vacaEtapa.getOcupaEspacio(),
-							// avanzamos la etapa
-							etapa + 1,
-							solucion
-					);
-				}
-
-				// si todavía no hemos encontrado la solución, seguimos buscando
-				if (!soluciónEncontrada) {
-					// probamos a no añadir la vaca
-					solucion.añadirVaca(null, etapa);
-
-					// probamos la posibilidad de no añadirla
-					soluciónEncontrada = runA(
-							vacasDisponibles,
-							lecheDeseada,
-							límiteEspacio,
-							// avanzamos la etapa
-							etapa + 1,
-							solucion
-					);
-				}
-			}
-		}
-
-		return soluciónEncontrada;
-	}
-
-	public static void runB (
-			final Vector<Vaca> vacasDisponibles,
+	public static void run (
+			final Vaca vacasDisponibles[],
 			final double lecheDeseada,
 			final int límiteEspacio,
 			final int etapa,
 			final Solucion solucion,
-			final Vector<Solucion> posiblesSoluciones
+			final Registro posiblesSoluciones
 	) {
 		// si es una posible solución
 		if (lecheDeseada <= 0) {
 			// guardamos una copia
-			posiblesSoluciones.add(new Solucion(solucion));
+			posiblesSoluciones.contemplarSolución(new Solucion(solucion));
 		}
 		
 		// si no quedan más vacas disponibles
-		if (etapa == vacasDisponibles.size()) {
-			// no se ha llegado a una solución por aquí
+		if (etapa == vacasDisponibles.length) {
+			// no avanzamos
 
 		// si quedan vacas disponibles
 		} else {
 			// obtenemos la vaca correspondiente
-			Vaca vacaEtapa = vacasDisponibles.elementAt(etapa);
-
-			// TODO: Comprobar si es mejor ir añadiendo las vacas "a la que volvemos", primero por la rama del null
+			Vaca vacaEtapa = vacasDisponibles[etapa];
 
 			// vemos si es viable
 			if (vacaEtapa.getOcupaEspacio() <= límiteEspacio) {
 				solucion.añadirVaca(vacaEtapa, etapa); 
 
-				runB(
+				run(
 						vacasDisponibles,
 						// actualizamos la cantidad de leche deseada
 						lecheDeseada - vacaEtapa.getProducciónLeche(),
@@ -104,36 +43,25 @@ public class Main {
 						solucion,
 						posiblesSoluciones
 				);
+
+				// exploramos la solución de no añadir la vaca
+				solucion.añadirVaca(null, etapa);
+
+				run(
+						vacasDisponibles,
+						lecheDeseada,
+						límiteEspacio,
+						// sólo avanzamos la etapa
+						etapa + 1,
+						solucion,
+						posiblesSoluciones
+				);
 			}
-
-			// probamos a no añadir la vaca
-			solucion.añadirVaca(null, etapa);
-
-			// probamos la posibilidad de no añadirla
-			runB(
-					vacasDisponibles,
-					lecheDeseada,
-					límiteEspacio,
-					// avanzamos la etapa
-					etapa + 1,
-					solucion,
-					posiblesSoluciones
-			);
+			// si no es viable ya no hay más posibilidades ya que están ordenadas por espacio,
+			// es decir, la siguiente ocupa lo mismo o más, por lo que tampoco va a entrar
 		}
 	}
 	
-	public static Solucion runC(final Vector<Solucion> posiblesSoluciones) {
-		Solucion mejor = posiblesSoluciones.elementAt(0);
-
-		for (final Solucion solu : posiblesSoluciones) {
-			if (mejor.getProducciónLeche() < solu.getProducciónLeche()) {
-				mejor = solu;
-			}
-		}
-
-		return mejor;
-	}
-
 	public static void main (String[] args) {
 		final String nombreFicheroDatos = lecturadatos.Constantes.PATHNAME_VACAS;
 		try {
@@ -141,23 +69,35 @@ public class Main {
 
 			lecturadatos.Fichero.leerVacas(nombreFicheroDatos, vacas);
 
-			final double lecheDeseada = lecturadatos.Usuario.obtenerLecheDeseada();
+			final Vaca vacasOrdenadas[] = new Vaca[vacas.size()];
+			for (int i = 0; i < vacas.size(); i++) {
+				vacasOrdenadas[i] = vacas.get(i);
+			}
+			
+			// ordenamos por consumo de espacio
+			campusvirtual.Ordenar.quickSortC(
+					vacasOrdenadas,
+					(Vaca o1, Vaca o2) -> {
+						return Integer.compare(o1.getOcupaEspacio(), o2.getOcupaEspacio());
+					}
+			);
+			
+			final Registro registro = new Registro();
 
-			Solucion solucion = new Solucion(vacas.size());
-
-			runA(vacas, lecheDeseada, lecturadatos.Constantes.ESPACIO_DISPONIBLE, 0, solucion);
-
-			System.out.println(solucion);
-			
-			final Vector<Solucion> posiblesSoluciones = new Vector<Solucion>(vacas.size());
-			
-			runB(vacas, lecheDeseada, lecturadatos.Constantes.ESPACIO_DISPONIBLE, 0, solucion, posiblesSoluciones);
-			
-			System.out.printf("Existen %d soluciones\n", posiblesSoluciones.size());
-			
-			final Solucion mejor = runC(posiblesSoluciones);
-			
-			System.out.println(mejor);
+			run(
+					vacasOrdenadas,
+					lecturadatos.Usuario.obtenerLecheDeseada(),
+					lecturadatos.Constantes.ESPACIO_DISPONIBLE,
+					// etapa
+					0,
+					// solución temporal para ir explorando
+					new Solucion(vacas.size()),
+					// contenedor de soluciones
+					registro
+			);
+		
+			// la primera solución no es 1, 2, 3... porque las habíamos ordenado por consumo de espacio
+			System.out.println(registro);
 
 		} catch (FileNotFoundException e) {
 			System.err.println("No se ha encontrado el fichero \"" + nombreFicheroDatos + "\".");
