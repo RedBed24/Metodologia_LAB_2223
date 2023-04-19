a7aa7c7f7d0e437a21a4098ef944d73086ea4e23
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-04-19 13:35:37 +0200

    Mejora entrada usuario Backtracking

--- Backtracking/src/lecturadatos/Usuario.java
+++ Backtracking/src/lecturadatos/Usuario.java
@@ -5,24 +5,22 @@ import java.util.Scanner;
 
 public class Usuario {
 
+	public static final Scanner TECLADO = new Scanner(System.in);
+
 	public static double obtenerLecheDeseada() {
-		final Scanner teclado = new Scanner(System.in);
-		
 		double lecheDeseada = -1;
 		do {
 			try {
 				System.out.println("¿Cuánta leche (en litros) se desea obtener?");
-				lecheDeseada = teclado.nextDouble();
-				if (lecheDeseada <= 0) {
+				lecheDeseada = TECLADO.nextDouble();
+				if (lecheDeseada < 0) {
 					System.err.println("Se necesita una cantidad de leche positiva.");
 				}
 			} catch (InputMismatchException e) {
-				teclado.nextLine();
+				TECLADO.nextLine();
 				System.err.println("Se espera un dato númerico.");
 			}
-		} while (lecheDeseada <= 0);
-		
-		teclado.close();
+		} while (lecheDeseada < 0);
 		
 		return lecheDeseada;
 	}
