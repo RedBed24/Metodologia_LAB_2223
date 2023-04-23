24b74d5866b237b55de2b7ff9608d2353ec1076f
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-04-23 10:11:46 +0200

    Add Backtracking/Main.runA executes only apartado A
    Algorithm stops when solution is found

--- Backtracking/src/Main.java
+++ Backtracking/src/Main.java
@@ -4,7 +4,65 @@ import java.util.NoSuchElementException;
 import vaca.Vaca;
 
 public class Main {
-	
+
+	public static Solucion runA(final Vaca vacasDisponibles[], final double lecheDeseada, final int límiteEspacio) {
+		final Solucion primera = new Solucion(vacasDisponibles.length);
+		runA(0, vacasDisponibles, lecheDeseada, límiteEspacio, primera);
+		return primera;
+	}
+
+	private static boolean runA(
+			final int etapa,
+			final Vaca vacasDisponibles[],
+			final double lecheDeseada,
+			final int límiteEspacio,
+			final Solucion solucion
+	) {
+		boolean solucionEncontrada = lecheDeseada <= 0;
+
+		// si no quedan más vacas sobre las que dedicir, hemos decidido por todas o ya hemos llegado a una solución
+		if (etapa == vacasDisponibles.length || solucionEncontrada) {
+			// paramos y volvemos
+
+		// si quedan vacas disponibles
+		} else {
+			// obtenemos la vaca correspondiente
+			Vaca vacaEtapa = vacasDisponibles[etapa];
+
+			// vemos si es viable
+			if (vacaEtapa.getOcupaEspacio() <= límiteEspacio) {
+				solucion.añadirVaca(vacaEtapa, etapa); 
+
+				solucionEncontrada = runA(
+						// avanzamos la etapa
+						etapa + 1,
+						vacasDisponibles,
+						// actualizamos la cantidad de leche deseada
+						lecheDeseada - vacaEtapa.getProducciónLeche(),
+						// actualizamos el espacio que gastamos
+						límiteEspacio - vacaEtapa.getOcupaEspacio(),
+						solucion
+				);
+			}
+
+			if (!solucionEncontrada) {
+				// exploramos la solución de no añadir la vaca
+				solucion.quitarVaca(etapa);
+
+				solucionEncontrada = runA(
+						// sólo avanzamos la etapa
+						etapa + 1,
+						vacasDisponibles,
+						lecheDeseada,
+						límiteEspacio,
+						solucion
+				);
+			}
+		}
+
+		return solucionEncontrada;
+	}
+
 	/**
 	 * <p>
 	 * Abstrae la primera llamada al algoritmo.
@@ -89,14 +147,20 @@ public class Main {
 		final String nombreFicheroDatos = lecturadatos.Constantes.PATHNAME_VACAS;
 		try {
 			final Vaca vacas[] = lecturadatos.Fichero.leerVacas(nombreFicheroDatos);
-			
+			final double lecheDeseada = lecturadatos.Usuario.obtenerLecheDeseada();
+			final int espacioDisponible = lecturadatos.Constantes.ESPACIO_DISPONIBLE;
+
+			final Solucion primera = runA(vacas, lecheDeseada, espacioDisponible);
+
+			System.out.printf("La primera solución encontrada es: %s.\n", primera);
+
 			// contenedor de soluciones
 			final Registro registro = new Registro();
 
 			run(
 					vacas,
-					lecturadatos.Usuario.obtenerLecheDeseada(),
-					lecturadatos.Constantes.ESPACIO_DISPONIBLE,
+					lecheDeseada,
+					espacioDisponible,
 					registro
 			);
 		
