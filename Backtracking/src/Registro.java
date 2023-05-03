019c9952cf9f5f40292e3bf425b6618a362ff302
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-05-03 16:04:45 +0200

    Add more javadoc to Registro

--- Backtracking/src/Registro.java
+++ Backtracking/src/Registro.java
@@ -2,15 +2,23 @@
 /**
  * <p>
  * Clase para llevar un registro de las soluciones encontradas.
+ * También se queda con aquella solución que sea mejor.
  * </p>
  * @author Noelia Díaz-Alejo Alejo, Samuel Espejo Gil 
  * @since 2023-03-20
  */
 public class Registro {
+	// representa el limitante del mínimo de leche que requerimos
 	private final double lecheDeseada;
 	private Solucion mejor;
 	private int cuentaSoluciones = 0;
 	
+	/**
+	 * <p>
+	 * Crea un nuevo Registro guardando el mínimo de leche que se requiere para que se consideren válidas las soluciones.
+	 * </p>
+	 * @param lecheDeseada Mínimo de leche, en las mismas unidades que las vacas.
+	 */
 	public Registro(final double lecheDeseada) {
 		super();
 		this.lecheDeseada = lecheDeseada;
